a373797741f2bd0f1e98ae1232f30b9cdf2a8d76
sangkwonkim <sangkwon2406@naver.com>
2022-03-15 11:17:08 +0900

    fix menu get api and testcode

--- server/controller/menu.js
+++ server/controller/menu.js
@@ -3,11 +3,26 @@ const { User: UserModel, AteMenu : AteMenuModel, Menu: MenuModel} = require('../
 module.exports = {
     get: async (req, res) => {
         try {
+            const styles = [ 'korean', 'japanese', 'chinese', 'western', 'others' ];
+            const types = [ 'rice', 'noodle', 'bread', 'others' ];
+            const query = req.query;
+            const spicy = query.spicy;
+            const meat = query.meat;
+            const soup = query.soup;
+            const style = query.style;
+            const type = query.type;
+            if(spicy && Number.isNaN(parseInt(spicy, 10))) return res.status(400).json({ message: 'spicy 요청이 잘 못 되었습니다.' });
+            if(meat && Number.isNaN(parseInt(meat, 10))) return res.status(400).json({ message: 'meat 요청이 잘 못 되었습니다.' });
+            if(soup && Number.isNaN(parseInt(soup, 10))) return res.status(400).json({ message: 'soup 요청이 잘 못 되었습니다.' });
+            if(style && !styles.includes(style)) return res.status(400).json({ message : 'style 요청이 잘 못 되었습니다.'})
+            if(type && !types.includes(type)) return res.status(400).json({ message : 'type 요청이 잘 못 되었습니다.'})
+
             const findMenus = await MenuModel.findAll({
                 where : req.query,
                 attributes : [ 'id', 'name' ],
                 order: [['id', 'ASC']],
             });
+            if( findMenus.length === 0 ) return res.status(404).json({ message: '조건에 충족하는 메뉴가 존재하지 않습니다.'});
             res.status(200).json({menus : findMenus})
         } catch {
             res.status(500).json({ message: '메뉴 찾기에 실패했습니다.' });
--- server/test/menuSpec.js
+++ server/test/menuSpec.js
@@ -54,22 +54,18 @@ describe('GET /user/login', () => {
             res.body.should.have.property('menus');
             res.body.menus[0].should.have.property('name', '김치찌개');
             done();
-        })
+        });
     });
-  });
-  describe('성공 시', () => {
     it('2. 요청 쿼리에 맞는 음식을 리턴한다.', (done) => {
       request(app)
         .get('/menu?style=korean')
         .end((err, res) => {
-            res.status.should.equal(200);
-            res.body.should.have.property('menus');
-            res.body.menus[1].should.have.property('name', '된장찌개');
-            done();
-        })
+          res.status.should.equal(200);
+          res.body.should.have.property('menus');
+          res.body.menus[1].should.have.property('name', '된장찌개');
+          done();
+        });
     });
-  });
-  describe('성공 시', () => {
     it('3. 요청 쿼리에 맞는 음식을 리턴한다.', (done) => {
       request(app)
       .get('/menu?spicy=0&meat=0&style=japanese')
@@ -78,10 +74,8 @@ describe('GET /user/login', () => {
             res.body.should.have.property('menus');
             res.body.menus[0].should.have.property('name', '초밥');
             done();
-        })
+        });
     });
-  });
-  describe('성공 시', () => {
     it('4. 요청 쿼리에 맞는 음식을 리턴한다.', (done) => {
       request(app)
       .get('/menu?style=chinese')
@@ -91,22 +85,76 @@ describe('GET /user/login', () => {
             res.body.menus[0].should.have.property('name', '짜장면');
             res.body.menus[1].should.have.property('name', '짬뽕');
             done();
-        })
+        });
     });
-  });
-  describe('성공 시', () => {
     it('5. 요청 쿼리가 없을 경우 모든 메뉴를 리턴한다.', (done) => {
       request(app)
       .get('/menu')
       .end((err, res) => {
-            res.status.should.equal(200);
-            res.body.should.have.property('menus');
-            console.log(res.body.menus)
-            res.body.menus[0].should.have.property('name', '김치찌개');
-            res.body.menus[2].should.have.property('name', '초밥');
-            res.body.menus[4].should.have.property('name', '짬뽕');
+        res.status.should.equal(200);
+        res.body.should.have.property('menus');
+        res.body.menus[0].should.have.property('name', '김치찌개');
+        res.body.menus[2].should.have.property('name', '초밥');
+        res.body.menus[4].should.have.property('name', '짬뽕');
+        done();
+      });
+    });
+  });
+  describe('실패 시', () => {
+    it('쿼리의 spicy가 숫자가 아닐 경우 400을 리턴한다.', (done) => {
+      request(app)
+        .get('/menu?spicy=one')
+        .end((err, res) => {
+            res.status.should.equal(400);
+            res.body.should.have.property('message', 'spicy 요청이 잘 못 되었습니다.');
             done();
-        })
+        });
+    });
+    it('쿼리의 meat가 숫자가 아닐 경우 400을 리턴한다.', (done) => {
+      request(app)
+        .get('/menu?meat=two')
+        .end((err, res) => {
+            res.status.should.equal(400);
+            res.body.should.have.property('message', 'meat 요청이 잘 못 되었습니다.');
+            done();
+        });
+    });
+    it('쿼리의 soup가 숫자가 아닐 경우 400을 리턴한다.', (done) => {
+      request(app)
+        .get('/menu?soup=three')
+        .end((err, res) => {
+            res.status.should.equal(400);
+            res.body.should.have.property('message', 'soup 요청이 잘 못 되었습니다.');
+            done();
+        });
+    });
+    it('쿼리의 style이 존재하지 않을 경우 400을 리턴한다.', (done) => {
+      request(app)
+        .get('/menu?style=italian')
+        .end((err, res) => {
+            res.status.should.equal(400);
+            res.body.should.have.property('message', 'style 요청이 잘 못 되었습니다.');
+
+            done();
+        });
+    });
+    it('쿼리의 type이 존재하지 않을 경우 400을 리턴한다.', (done) => {
+      request(app)
+        .get('/menu?type=juice')
+        .end((err, res) => {
+            res.status.should.equal(400);
+            res.body.should.have.property('message', 'type 요청이 잘 못 되었습니다.');
+            done();
+        });
+    });
+    it('요청에 맞는 메뉴가 없을 경우 404를 리턴한다.', (done) => {
+      request(app)
+        .get('/menu?spicy=1&meat=1&soup=1&style=japanese&type=bread')
+        .end((err, res) => {
+            res.status.should.equal(404);
+            res.body.should.have.property('message', '조건에 충족하는 메뉴가 존재하지 않습니다.');
+            done();
+        });
     });
   });
 });
