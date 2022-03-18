8372f4d9712c9b35ef101c24f7064ecc0e500a99
sangkwonkim <sangkwon2406@naver.com>
2022-03-18 23:41:37 +0900

    client routing setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <Nav /> */}
+      <Switch>
+        <Route exact path='/'>
+        </Route>
+        <Route path='/question'>
+        </Route>
+        <Route path='/login'>
+        </Route>
+        <Route path='/signup'>
+        </Route>
+        <Route path='/myPage'>
+        </Route>
+        <Route path='/map'>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
