336c975704e289a19a9bc4a0b00e17d29fec4391
sangkwonkim <sangkwon2406@naver.com>
2022-03-21 13:46:03 +0900

    main component setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './components/Main/Main'
 
 function App() {
   return (
     <Router>
-      {/* <Nav /> */}
-      <Switch>
+      <Main />
+      <Routes>
         <Route exact path='/'>
         </Route>
         <Route path='/question'>
@@ -19,7 +20,7 @@ function App() {
         </Route>
         <Route path='/map'>
         </Route>
-      </Switch>
+      </Routes>
     </Router>
   );
 }
