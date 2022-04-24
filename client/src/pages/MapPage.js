5292ce838182907bc83e79a6863b9ba5d5579afe
sangkwonkim <sangkwon2406@naver.com>
2022-04-24 15:10:35 +0900

    Add my location display function in kakao map

--- client/src/pages/MapPage.js
+++ client/src/pages/MapPage.js
@@ -9,12 +9,48 @@ const Map = () => {
 
     let options = {
       center: new kakao.maps.LatLng(35.85133, 127.734086),
-      level: 13,
+      level: 9,
     };
 
     let map = new kakao.maps.Map(container, options);
 
-    console.log("loading kakaomap");
+    function displayMarker(locPosition, message) {
+
+      // 마커를 생성합니다
+      var marker = new kakao.maps.Marker({  
+          map: map, 
+          position: locPosition
+      }); 
+      
+      var iwContent = message, // 인포윈도우에 표시할 내용
+          iwRemoveable = true;
+  
+      // 인포윈도우를 생성합니다
+      var infowindow = new kakao.maps.InfoWindow({
+          content : iwContent,
+          removable : iwRemoveable
+      });
+      
+      // 인포윈도우를 마커위에 표시합니다 
+      infowindow.open(map, marker);
+      
+      // 지도 중심좌표를 접속위치로 변경합니다
+      map.setCenter(locPosition);      
+  }    
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        
+      var lat = position.coords.latitude, // 위도
+          lon = position.coords.longitude; // 경도
+      
+      var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+          message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+      
+      // 마커와 인포윈도우를 표시합니다
+      displayMarker(locPosition, message);
+          
+    });
+    
   }, []);
 
   return (
